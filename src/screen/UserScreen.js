c6543f6a52655756989efaf3c7fdbdcbb6166ebe
aliraza0001 <aliarain214@gmail.com>
2020-01-15 12:38:56 +0500

    user profile style done

--- src/screen/UserScreen.js
+++ src/screen/UserScreen.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import User from '../components/profile/index'
+import SearchBar from '../components/searchbar/index'
 
 export default class UserScreen extends Component {
     render() {
         return (
-            <View>
-                <Text> textInComponent </Text>
+            <View style={{ backgroundColor: "#23152e", flex: 1 }}>
+                <User />
+                <SearchBar />
             </View>
         )
     }
