19ee7675cf76ab66f7ad813b1c3a7083b364bd94
aliraza0001 <aliarain214@gmail.com>
2020-01-16 15:17:04 +0500

    stack navigation work done

--- src/Navigation/stackNavigation.js
+++ src/Navigation/stackNavigation.js
@@ -1,6 +1,15 @@
-// import {createStackNavigator} from 'react-navigation-stack';
+import {createStackNavigator} from 'react-navigation-stack';
+import UserScreen from '../screen/UserScreen';
+import userChatScreen from '../screen/userChatScreen';
 
 
-// export default createStackNavigator({
-    
-// })
+export default  createStackNavigator({
+    Home:{
+      screen:UserScreen,
+      navigationOptions:{
+        headerShown:false
+      }
+    },
+    UserChat:userChatScreen
+  
+  })
